a7022cd490b975eb530ac63633c27e1e04463314
aagraston <aagraston@gmail.com>
2022-01-17 21:02:51 -0800

    super proud of my grid being added, especially figuring out to change box-sizing attribute of the grid and elements

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-let gridWidth = 16;
-let gridHeight = 16;
+let gridWidth = 50;
+let gridHeight = 50;
+
+let percentageMod = (1/gridWidth) * 100;
+
 
 const gridContainer = document.querySelector('.grid-container');
 
 for (let i = 0; i < (gridWidth * gridHeight); i++) {
   let gridElement = document.createElement('div');
+  
   gridElement.classList.add('grid-item');
+  gridElement.style.cssText += `flex-basis: ${percentageMod}%;`;
+
+  gridElement.addEventListener('pointerover', hoverGridElement);
+
   gridContainer.appendChild(gridElement);
+}
+
+function hoverGridElement() {
+  this.classList.add('grid-item-hovered');
 }
 
 console.log(gridContainer);
